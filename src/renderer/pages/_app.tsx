02d06bcd7d200468cb3c735554cef581ea3c401d
vegedot <vegedot.tt@gmail.com>
2021-10-31 20:52:31 +0900

    feat: add destyle.css (reset css)

---
+++ src/renderer/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "destyle.css";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
